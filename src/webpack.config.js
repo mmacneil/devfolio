68365a96bc6403776202dddb64065ac852cb9553
mmacneil <markmacneil@gmail.com>
2019-08-16 00:38:14 -0300

    Wip

---
+++ src/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              // options...
+            }
+          }
+        ]
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].bundle.css'
+    }),
+  ]
+};
