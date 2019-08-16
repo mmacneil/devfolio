6289926be5176bcc124dfd5353093e39b75b47dc
mmacneil <markmacneil@gmail.com>
2019-08-16 00:59:32 -0300

    Wip

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  // https://webpack.js.org/plugins/mini-css-extract-plugin/#minimizing-for-production
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.js'
