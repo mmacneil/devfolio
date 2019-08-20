253a2306c25a86adb43bf1a3ba1e24c7a446aef4
mmacneil <markmacneil@gmail.com>
2019-08-20 01:11:17 -0300

    Project modals wip.

--- src/src/index.js
+++ src/src/index.js
@@ -1,1 +1,45 @@
 require('./styles.scss');
+
+
+// Modals
+
+var rootEl = document.documentElement;
+var $modals = getAll('.modal');
+var $modalTriggers = getAll('.modal-trigger');
+var $modalCloses = getAll('.modal-card-head .delete, .modal-card-foot .button');
+
+if ($modalTriggers.length > 0) {
+    $modalTriggers.forEach(function ($el) {
+        $el.addEventListener('click', function () {
+            var target = $el.dataset.target;
+            openModal(target);
+        });
+    });
+}
+
+if ($modalCloses.length > 0) {
+    $modalCloses.forEach(function ($el) {
+        $el.addEventListener('click', function () {
+            closeModals();
+        });
+    });
+}
+
+function openModal(target) {
+    var $target = document.getElementById(target);
+    rootEl.classList.add('is-clipped');
+    $target.classList.add('is-active');
+}
+
+function closeModals() {
+    rootEl.classList.remove('is-clipped');
+    $modals.forEach(function ($el) {
+        $el.classList.remove('is-active');
+    });
+}
+
+// Functions
+
+function getAll(selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+}
